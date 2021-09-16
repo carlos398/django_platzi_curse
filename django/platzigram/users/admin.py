952683b9c1b06a189755d0ce1d5e11371fc9c831
carlos_reyes <carlosarp2000@gmail.com>
2021-09-15 21:48:06 -0500

    cambios en admin y agrego perfil

--- django/platzigram/users/admin.py
+++ django/platzigram/users/admin.py
@@ -12,3 +12,17 @@ class ProfileAdmin(admin.ModelAdmin):
     """profile admin."""
 
     list_display = ('user', 'phone_number', 'website', 'picture')
+    search_fields = (
+        'user__email', 
+        'user__username',
+        'user__first_name', 
+        'user__last_name', 
+        'phone_number'
+        )
+    
+    list_filter = (
+        'created', 
+        'modified',
+        'user__is_active',
+        'user__is_staff'
+        )
