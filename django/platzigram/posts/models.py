2d82dd11ed025ac69cb19db2ff1ad16755b1df11
carlos_reyes <carlosarp2000@gmail.com>
2021-09-23 21:08:39 -0500

    modelo de bd post creado

---
+++ django/platzigram/posts/models.py
@@ -0,0 +1,25 @@
+"""Posts models."""
+
+#Django
+from users.models import Profile
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    """POST MODEL."""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='post/photos')
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return title and username"""
+        return ' {} by @{}'.format(
+            self.title,
+            self.user.username
+        )
--- django/platzigram/users/admin.py
+++ django/platzigram/users/admin.py
@@ -1,11 +1,11 @@
 
 # Models
-from django.contrib.admin.decorators import display
+from django.contrib.auth.models import User
 from users.models import Profile
 
 #Django
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
@@ -38,3 +38,30 @@ class ProfileAdmin(admin.ModelAdmin):
             )
         })
     )
+
+
+class ProfileInline(admin.StackedInline):
+    """profile in-line admin for users"""
+
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin"""
+
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
