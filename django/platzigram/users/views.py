995d2d90e0719f44b09edbeb5fa9d8a0a0c37d84
carlos_reyes <carlosarp2000@gmail.com>
2021-09-28 22:30:34 -0500

    mejora en las vistas de login y logout creado

--- django/platzigram/users/views.py
+++ django/platzigram/users/views.py
@@ -1,7 +1,8 @@
 """users views"""
 
 #Django
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
 def login_view(request):
@@ -17,3 +18,10 @@ def login_view(request):
             return render(request,'users/login.html', {'error': 'invalid username and password'})
 
     return render(request, 'users/login.html')
+
+
+@login_required
+def logout_vew(request):
+    """logout a user"""
+    logout(request)
+    return redirect('login')
