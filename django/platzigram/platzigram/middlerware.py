d9a8fd41b2bd4a1703c7521e1cc185e053786b58
carlos_reyes <carlosarp2000@gmail.com>
2021-10-02 12:00:49 -0500

    manejo de vistas y completado de perfil

--- django/platzigram/platzigram/middlerware.py
+++ django/platzigram/platzigram/middlerware.py
@@ -2,7 +2,7 @@
 
 #Django
 from django.shortcuts import redirect
-
+from django.urls import reverse
 
 
 class ProfileCompletionMiddleware:
@@ -24,7 +24,8 @@ class ProfileCompletionMiddleware:
         if not request.user.is_anonymous:
             profile = request.user.profile
             if not profile.picture or not profile.biography:
-                return redirect('update_profile')
+                if request.path not in [reverse('update_profile'), reverse('logout')]:
+                    return redirect('update_profile')
 
         response = self.get_response(request)
         return response
