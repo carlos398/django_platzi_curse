986d3d27d6ee3dd958bf1b1b5110b0e3c2a5f663
carlos_reyes <carlosarp2000@gmail.com>
2021-10-05 22:12:23 -0500

    form de signup modificaciones

--- django/platzigram/users/forms.py
+++ django/platzigram/users/forms.py
@@ -3,6 +3,30 @@
 # Django
 from django import forms
 
+# models
+from django.contrib.auth.models import User
+
+class SignupForm(forms.Form):
+    """Sign up form"""
+
+    username = forms.CharField(min_leng=4, max_length=50)
+
+    password = forms.CharField(max_length=70, widget=forms.PasswordInput)
+    password_confirmation = forms.CharField(max_length=70, widget=forms.PasswordInput)
+
+    firs_name = forms.CharField(min_length=2, max_length=50)
+    last_name = forms.CharField(min_length=2, max_length=50)
+
+    email = forms.CharField(min_length=6, max_length=70, widget=forms.EmailInput)
+
+    def clean_username(self):
+        """username must be unique"""
+        username = self.cleaned_data['username']
+        username_in_use = User.object.filter(username=username).exist()
+        if username_in_use:
+            raise forms.ValidationError('username is already in use')
+        return username
+
 
 class ProfileForm(forms.Form):
     """Profile form."""
