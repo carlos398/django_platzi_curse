f70df61593b6c1a6c929acabcadfd0be21d37e7c
carlos_reyes <carlosarp2000@gmail.com>
2021-10-07 20:27:54 -0500

    correccion min_length

--- django/platzigram/users/forms.py
+++ django/platzigram/users/forms.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 class SignupForm(forms.Form):
     """Sign up form"""
 
-    username = forms.CharField(min_leng=4, max_length=50)
+    username = forms.CharField(min_length=4, max_length=50)
 
     password = forms.CharField(max_length=70, widget=forms.PasswordInput)
     password_confirmation = forms.CharField(max_length=70, widget=forms.PasswordInput)
